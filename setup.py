b322836789de3b93cb049e8cb34e5e43402a6642
Jeremy Smith <j.smith.03@cantab.net>
2017-05-17 10:57:51 -0700

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name = 'NLStats',
+    version = '1.0',
+    author = 'Jeremy Smith',
+    author_email = 'j.smith.03@cantab.net',
+    url = 'http://jeremysmithscientist.com',
+    py_modules = ['NLStats'],
+    )
